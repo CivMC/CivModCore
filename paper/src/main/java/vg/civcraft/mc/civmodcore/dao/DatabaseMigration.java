5a913b710b3f9c003687eae8c8b5b5ebe2da3f68
Alexander <protonull@protonmail.com>
2022-05-11 22:42:39 +0100

    Fix DatabaseMigration annotations
    Seemingly, at some point, Java changed how it interprets annotations on types: `@NotNull String[]` is now considered to be a nullable array of non-null strings. And so `String @NotNull []` is required to indicate a non-null array of nullable strings.

--- paper/src/main/java/vg/civcraft/mc/civmodcore/dao/DatabaseMigration.java
+++ paper/src/main/java/vg/civcraft/mc/civmodcore/dao/DatabaseMigration.java
@@ -1,8 +1,8 @@
 package vg.civcraft.mc.civmodcore.dao;
 
 import java.sql.SQLException;
-import javax.annotation.Nonnull;
 import org.apache.commons.lang3.ArrayUtils;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Interface to allow for more object-oriented based migration. I've noticed as plugins get older, their DAOs get
@@ -27,23 +27,22 @@ public interface DatabaseMigration {
 	/**
 	 * @return Returns this migration's queries. Each query will be run in sequences. Must not be null or empty!
 	 */
-	@Nonnull
-	String[] getMigrationQueries();
+	@NotNull String @NotNull [] getMigrationQueries();
 
 	/**
 	 * An optional callback that'll run after the migration has completed.
 	 *
 	 * @return Returns whether the callback completed successfully.
 	 */
-	default boolean migrationCallback(@Nonnull final ManagedDatasource datasource) throws SQLException {
+	default boolean migrationCallback(@NotNull final ManagedDatasource datasource) throws SQLException {
 		return true;
 	}
 
 	/**
 	 * @param datasource The datasource to register this migration to.
 	 */
-	default void registerMigration(@Nonnull final ManagedDatasource datasource) {
-		final var queries = getMigrationQueries();
+	default void registerMigration(@NotNull final ManagedDatasource datasource) {
+		final String[] queries = getMigrationQueries();
 		if (ArrayUtils.isEmpty(queries)) {
 			throw new IllegalArgumentException("Migration queries cannot be null or empty!");
 		}
