9730e437c0120a8a599a3cbbbd7700a7b2154c8b
Alexander <protonull@protonmail.com>
2022-05-11 22:56:37 +0100

    Update junit to 5.8.2

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -23,8 +23,8 @@ dependencies {
 	compileOnly("org.projectlombok:lombok:1.18.24")
 	annotationProcessor("org.projectlombok:lombok:1.18.24")
 
-	testImplementation("org.junit.jupiter:junit-jupiter-api:5.2.0")
-	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.2.0")
+	testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
 tasks {
