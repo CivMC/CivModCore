cd405f19a9609c9af654df2983a8d2d26d489750
Alexander <protonull@protonmail.com>
2022-05-13 06:33:11 +0100

    Simplify NBTSerializationException
    Goodness gracious... using Lombok's StandardException here sure is obfuscating implementation details and will surely make debugging harder.

--- paper/src/main/java/vg/civcraft/mc/civmodcore/nbt/NBTSerializationException.java
+++ paper/src/main/java/vg/civcraft/mc/civmodcore/nbt/NBTSerializationException.java
@@ -1,31 +1,17 @@
 package vg.civcraft.mc.civmodcore.nbt;
 
 import java.io.Serial;
+import lombok.experimental.StandardException;
 import vg.civcraft.mc.civmodcore.nbt.wrappers.NBTCompound;
 
 /**
  * Exception that ought to be used within {@link NBTSerializable#toNBT(NBTCompound)} and
  * {@link NBTSerializable#fromNBT(NBTCompound)}.
  */
+@StandardException
 public class NBTSerializationException extends RuntimeException {
 
 	@Serial
 	private static final long serialVersionUID = 606023177729327630L;
-
-	public NBTSerializationException() {
-		super();
-	}
-
-	public NBTSerializationException(String message) {
-		super(message);
-	}
-
-	public NBTSerializationException(String message, Throwable cause) {
-		super(message, cause);
-	}
-
-	public NBTSerializationException(Throwable cause) {
-		super(cause);
-	}
 
 }
