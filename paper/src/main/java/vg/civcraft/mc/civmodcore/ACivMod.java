6eae7c52636592c129bd26ae9cf861347d7c6920
Alexander <protonull@protonmail.com>
2022-05-13 06:34:21 +0100

    Remove redundant calls from ACivMod::onDisable

--- paper/src/main/java/vg/civcraft/mc/civmodcore/ACivMod.java
+++ paper/src/main/java/vg/civcraft/mc/civmodcore/ACivMod.java
@@ -15,7 +15,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.configuration.serialization.ConfigurationSerializable;
 import org.bukkit.configuration.serialization.ConfigurationSerialization;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.PluginDisableEvent;
 import org.bukkit.plugin.Plugin;
@@ -43,10 +42,6 @@ public abstract class ACivMod extends JavaPlugin {
 	
 	@Override
 	public void onDisable() {
-		HandlerList.unregisterAll(this);
-		Bukkit.getMessenger().unregisterIncomingPluginChannel(this);
-		Bukkit.getMessenger().unregisterOutgoingPluginChannel(this);
-		Bukkit.getScheduler().cancelTasks(this);
 		this.configClasses.forEach(ConfigurationSerialization::unregisterClass);
 	}
 
